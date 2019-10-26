2e3a332c0da02d3e86e5baac0ad5bcfe0b0655e2
Cavanaugh Richards <cavatron@pm.me>
2019-10-26 17:00:06 -0500

    add another webhook for form submission

--- src/lambda/postForm.js
+++ src/lambda/postForm.js
@@ -4,11 +4,18 @@ export async function handler(event) {
   try {
     const body = JSON.parse(event.body);
 
-    const response = await axios.post(process.env.VUE_APP_WEBHOOK_URL, body);
+    const emailRes = await axios.post(process.env.VUE_APP_WEBHOOK_EMAIL, body);
+    const spreadsheetRes = await axios.post(
+      process.env.VUE_APP_WEBHOOK_SPREADSHEET,
+      body
+    );
 
     return {
       statusCode: 200,
-      body: JSON.stringify(response.data)
+      body: JSON.stringify({
+        email: emailRes.data,
+        sheet: spreadsheetRes.data
+      })
     };
   } catch (error) {
     return {
